2303cbaa6e381d113ea578acb8aaadad7a28cba6
Victor <victor_weazelnews@outlook.com>
2021-06-08 18:55:58 -0500

    Agregado template de Empresa y Galeria y la carpeta de imagenes

--- main.go
+++ main.go
@@ -25,9 +25,19 @@ func conexionBD() (conexion *sql.DB) {
 
 func main() {
 	http.HandleFunc("/", Inicio)
+	http.HandleFunc("/empresa", Empresa)
+	http.HandleFunc("/galeria", Galeria)
 	log.Println("Servidor Encendido...")
 	http.ListenAndServe(":8080", nil)
 }
 func Inicio(w http.ResponseWriter, r *http.Request) {
 	plantillas.ExecuteTemplate(w, "inicio", nil)
+}
+
+func Empresa(w http.ResponseWriter, r *http.Request){
+	plantillas.ExecuteTemplate(w, "empresa", nil)
+}
+
+func Galeria(w http.ResponseWriter, r *http.Request){
+	plantillas.ExecuteTemplate(w, "galeria", nil)
 }
